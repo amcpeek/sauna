e38004908a703560bc257b6bc4dc6261a3687ba9
amcpeek <ammcpeek@gmail.com>
2023-01-24 14:52:58 -0800

    changed quote style for render

--- app/seeds/projects.py
+++ app/seeds/projects.py
@@ -1,20 +1,26 @@
 from app.models import db, Project, environment, SCHEMA
 
-
 def seed_projects():
     project1=Project(ownerId=1,name='Implementing new NGSS standards aligned science curriculum',
     description='With the newly created federal Next Generation Science Standards, Oakland Unified School District needs to select a new standards aligned curriculum and implement it.')
     project2=Project(ownerId=1, name='Applying for the State funding to create a STEM lab at Hoover Elementary',
-    description='''The state of California has allocated funds to STEM labs in low income schools, but in order to receive it we need to create and implement a detailed plan of how we
-    will effectively use the funds ''')
+    description='The state of California has allocated funds to STEM labs in low income schools, but in order to receive it we need to create and implement a detailed plan of how we will effectively use the funds')
     project3=Project(ownerId=2, name='Piloting a new remote literacy tutoring project',
-    description="""Partner with an external tutor provider company to have their tutors working with Oakland students using our company's curriculum""")
+    description='Partner with an external tutor provider company to have their tutors working with Oakland students using our companys curriculum')
     project4=Project(ownerId=2, name='Hire, onboard, and train a team of implementation specialist',
-    description="""Work with HR to create a hiring, onboarding, and continual coaching process.""")
+    description='Work with HR to create a hiring, onboarding, and continual coaching process.')
     db.session.add(project1)
     db.session.add(project2)
     db.session.add(project3)
     db.session.add(project4)
+    db.session.commit()
+
+def undo_projects():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute("DELETE FROM projects")
+
     db.session.commit()
 
 # Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
@@ -23,10 +29,3 @@ def seed_projects():
 # incrementing primary key, CASCADE deletes any dependent entities.  With
 # sqlite3 in development you need to instead use DELETE to remove all data and
 # it will reset the primary keys for you as well.
-def undo_projects():
-    if environment == "production":
-        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
-    else:
-        db.session.execute("DELETE FROM projects")
-
-    db.session.commit()
--- app/seeds/tasks.py
+++ app/seeds/tasks.py
@@ -4,21 +4,20 @@ from app.models import db, Task, environment, SCHEMA
 def seed_tasks():
     task1=Task(projectId=1, name='Curriculum Reviewing Event',
     description='Invite teachers across the district to review curriculum at the science dept' )
-    task2=Task(projectId=1, name="""School Board Meeting to Select Curriculum""",
-    description="""Analyze the result from the teacher curriculum viewing event to create a slide deck for the
-    school board meeting""" )
-    task3=Task(projectId=1, name="""Finalize purchase of curriculum with curriculum company""",
-    description="""Go over the budge internally with the district and sign contract with the curriculum company""" )
-    task4=Task(projectId=1, name="""Plan for teacher leaders to be trained in the new curriculum""",
-    description="""Work with school district to create a summer training program for teacher leaders and fall training for all teachers""" )
-    task5=Task(projectId=2, name="""Review grant application""",
-    description="""Evaluate if our school can meet the restrictions of the grant application, and if funds are worth the expense of applying for the grant""" )
-    task6=Task(projectId=2, name="""Create budget for grant application""",
-    description="""Develop a list of all items we would like to purchase with the grant funds that meet the requirements of the grant""" )
-    task7=Task(projectId=2, name="""Get permission from parents to include photos of students for the grant application""",
-    description="""Photos are required for the application, and the form to get parents to consent is on their website""" )
-    task8=Task(projectId=2, name="""Write the application content and get the grant reviewed""",
-    description="""Ask another team member to review the content of the application.""" )
+    task2=Task(projectId=1, name='School Board Meeting to Select Curriculum',
+    description='Analyze the result from the teacher curriculum viewing event to create a slide deck for the school board meeting' )
+    task3=Task(projectId=1, name='Finalize purchase of curriculum with curriculum company',
+    description='Go over the budge internally with the district and sign contract with the curriculum company' )
+    task4=Task(projectId=1, name='Plan for teacher leaders to be trained in the new curriculum',
+    description='Work with school district to create a summer training program for teacher leaders and fall training for all teachers' )
+    task5=Task(projectId=2, name='Review grant application',
+    description='Evaluate if our school can meet the restrictions of the grant application, and if funds are worth the expense of applying for the grant' )
+    task6=Task(projectId=2, name='Create budget for grant application',
+    description='Develop a list of all items we would like to purchase with the grant funds that meet the requirements of the grant' )
+    task7=Task(projectId=2, name='Get permission from parents to include photos of students for the grant application',
+    description='Photos are required for the application, and the form to get parents to consent is on their website' )
+    task8=Task(projectId=2, name='Write the application content and get the grant reviewed',
+    description='Ask another team member to review the content of the application.' )
     db.session.add(task1)
     db.session.add(task2)
     db.session.add(task3)

bd344b37bbbddfec35d0ee860086abb3b08fd2a5
amcpeek <ammcpeek@gmail.com>
2023-01-24 19:55:15 -0800

    Render Fix 0755

--- app/forms/project_form.py
+++ app/forms/project_form.py
@@ -7,3 +7,5 @@ from app.models import Project
 class ProjectForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     description = StringField('description', validators=[DataRequired()])
+
+

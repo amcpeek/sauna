f3cc5186e474fd1b6f59a14c56bdca1de98e1fb5
amcpeek <ammcpeek@gmail.com>
2023-01-30 22:02:08 -0800

    before deleting migrations folder

--- app/models/team.py
+++ app/models/team.py
@@ -8,15 +8,27 @@ class Team(db.Model):
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
 
-    id = db.Column(db.Integer, primary_key=True)
+    # home_id = db.Column(db.Integer, db.ForeignKey('Teams.id'), nullable=False)
+    # away_id = db.Column(db.Integer, db.ForeignKey('Teams.id'), nullable=False)
+
+    # home_ref = db.relationship("Teams", backref="fixture", uselist=False, foreign_keys=[home_id])
+    # away_ref = db.relationship("Teams", backref="fixture", uselist=False, foreign_keys=[away_id])
+
+
+
+
     ownerId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")))
     memberId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")))
+    user = db.relationship("User", back_populates="teams", foreign_keys=[ownerId])
+    user = db.relationship("User", back_populates="teams", foreign_keys=[memberId])
 
+
+    id = db.Column(db.Integer, primary_key=True)
     projectId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("projects.id")))
     name = db.Column(db.String(50), nullable=False)
     description = db.Column(db.String(255), nullable=False)
 
-    user = db.relationship("User", back_populates="teams", uselist=False, foreign_keys=[ownerId, memberId])
+
     projects = db.relationship("Project", back_populates='team', cascade="all, delete")
 
     def to_dict(self):

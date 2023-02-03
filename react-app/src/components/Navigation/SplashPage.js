a8c7a0f36cc9d6d4019cba8e6e49277e18ce90a7
amcpeek <ammcpeek@gmail.com>
2023-02-03 12:41:26 -0800

    updating splash page

--- react-app/src/components/Navigation/SplashPage.js
+++ react-app/src/components/Navigation/SplashPage.js
@@ -10,14 +10,28 @@ const SplashPage = () => {
         <div className='f'>
                         <div className='main-left col main-left lr-margin'>
 
-                            <div>
+                            <div className='lr-margin-small col  width-80-per'>
                                 <h1>Are silos making teamwork <br/> more painful?</h1>
                                 <h4>Sauna, an Asana clone,  helps you manage projects, focus on what's important, <br/> and organize work in one place for seamless collaboration.</h4>
-                                <h4>Join one of the many teams working on projects on Sauna </h4>
-                                <h4>Join our sample company managing teams and projects with Sauna </h4>
-                                <h2><Link className='cursor text-blue just-no-und' to='/teams'>Discover Teams</Link></h2>
-
                             </div>
+
+
+                            <div className='lr-margin-small col width-80-per'>
+                                <h1>Stay organized and <br/> connected</h1>
+                                <h4>Bring your team's work together in one shared space. Choose the project view that suits your style, and collaborate no matter where you are.</h4>
+                            </div>
+
+                            <Link className='cursor text-blue just-no-und ' to='/teams'>
+                            <div className='asana-button width-60-per lr-margin-small'>
+                                <div className='lr-margin-small col'>
+                                <h1>Join our sample company </h1>
+                                <h4>Manage teams, projects, and tasks with Sauna </h4>
+                                </div>
+                            </div>
+
+                            </Link>
+
+
 
                         </div>
        <div className='main-gray-box'>

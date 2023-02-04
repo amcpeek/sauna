c472b74bac20f5f56cc45d0050f764ca89f287d5
amcpeek <ammcpeek@gmail.com>
2023-02-04 15:00:18 -0800

    fix project page, not done with overview formatting

--- react-app/src/components/Projects/ViewProject.js
+++ react-app/src/components/Projects/ViewProject.js
@@ -23,6 +23,9 @@ const ViewProject = () => {
     const [showAddTask2, setShowAddTask2] = useState(false)
     const [showAddTask3, setShowAddTask3] = useState(false)
     const [showModal, setShowModal] = useState(false);
+    const [overView, setOverView] = useState(false)
+    const [boardView, setBoardView] = useState(true)
+    const [listView, setListView] = useState(false)
     let arr = []
     let isMember = ''
     let memberArray = []
@@ -106,7 +109,7 @@ const ViewProject = () => {
         const complete = arr.filter(task => task.stageId == 3)
         return (
             <div className='col'>
-                <div className='col vh-20 lr-margin-small'>
+                <div className='col vh-5 lr-margin-small'>
                     {/* <div><Link to={'/'}><i className="fa-solid fa-house-chimney"></i></Link></div>
                     <div className='bg-green small-box round-sq'> AM</div> */}
 
@@ -114,13 +117,7 @@ const ViewProject = () => {
                     <div className='solid-round-sq jc-c ai-c' style={{backgroundColor: arrayOfColors[oneProject.id]}}><i className="fa-solid fa-list-ul"></i></div>&nbsp;&nbsp;
                         <h2>{oneProject.name}</h2>
                         </div>
-                    <div className='should-wrap-full scroller'>
-                       <p className='font-small-med'>Project Lead: {oneProject.owner.username}<br/>
 
-                       <p>Team: {oneTeam.name}</p>
-                         {oneProject.description} </p>
-                        </div>
-                    <div className='long-gray-line tb-margin'></div>
                     {/* <div><Link to={`/projects/${id}/edit`}>Edit Project</Link></div> */}
 
 
@@ -139,9 +136,9 @@ const ViewProject = () => {
                      </div>
                     }
 
-                    <button className='just-text-button do-not-interact bg-white'>Overview</button>
-                    <button className='just-text-button do-not-interact bg-white'>Board</button>
-                    <button className='just-text-button do-not-interact bg-white'>List</button>
+                    <button onClick={() => (setListView(false),setBoardView(false), setOverView(true))} className='just-text-button  bg-white'>Overview</button>
+                    <button onClick={() => (setListView(false), setOverView(false), setBoardView(true))}  className='just-text-button bg-white'>Board</button>
+                    <button onClick={() => (setBoardView(false), setOverView(false), setListView(true))}  className='just-text-button  bg-white'>List</button>
                     {oneTeam && oneTeam.memberships && user && memberArray && !isMember && (
                         <div className='row ai-c'>
 
@@ -156,121 +153,138 @@ const ViewProject = () => {
 
                 <div className='bg-light-gray round-sq-05 vw-99-vh-70 scroller-tasks'>
 
-                <div className='f width-100-per'>
-                    <div className='col width-40-per lr-margin-small'>
-                        <h2>To Do
-                            {isMember && (
-                             <button className='match-tasks cursor' onClick={() => (showAddTask1? setShowAddTask1(false): setShowAddTask1(true))}><i className="fa-solid fa-plus "></i></button>
-                            )}
+                    {/* ONLY SHOWS IF OVERVIEW IS TRUE */}
 
-                            </h2>
-                        {toDo.map(task=> {
-                            return (
-                                <button className="f just-text-button b-margin bg-white round-sq-05 height-task cursor pad--1" key={task.id} onClick={() => (showTaskFunc(task))}>
-                                    <i className="fa-regular fa-circle-check"></i>&nbsp;&nbsp;
-                                    {task.name}
-                                </button>
-                            )
-                        })}
-                        <div  className="jc-end"
-                        // style={!showAddTask ? { transform: 'translateX(+100%)' } : {}}
-                        >
-                            {showAddTask1 &&
-                            <button className=" width-100-per bg-white just-text-button round-sq-05 box-shadow b-margin">
-                        <CreateTask setShowAddTask1={setShowAddTask1} showAddTask1={showAddTask1}/>
-                        {/* this prop thing isnt working yet, doesnt seem to be doing anything actually */}
-                        </button>
-                            }
 
+                    {overView && (
+                         <div className='f width-100-per lr-margin-small'>
+                             <div className='should-wrap-full scroller'>
+                            <p className='font-small-med'>Project Lead: {oneProject.owner.username}<br/>
+
+                            <p>Team: {oneTeam.name}</p>
+                                {oneProject.description} </p>
+                                </div>
+                            <div className='long-gray-line tb-margin'></div>
+
+                         </div>
+
+                    )}
+
+                    {/* ONLY SHOWS IF LIST IS TRUE */}
+
+                    {listView && (
+                        <div className='f width-100-per jc-c'>
+                            <h2 className='lr-margin jc-c'>This feature is still in development</h2>
                         </div>
-
-                    </div>
-
+                    )}
 
 
-                    <div className='col width-40-per lr-margin-small'>
-                        <h2>In Progress
-                        {isMember && (<button className='just-text-button match-tasks' onClick={() => ( showAddTask2? setShowAddTask2(false): setShowAddTask2(true))}><i className="fa-solid fa-plus"></i></button>
-                        )}
+                    {/* ONLY SHOWS IF BOARD IS TRUE */}
 
-                            </h2>
-                        {inProg.map(task=> {
-                            return (
-                                <button className="f just-text-button b-margin bg-white round-sq-05 height-task cursor pad--1" key={task.id} onClick={() => (showTaskFunc(task)) }>
-                                    <i className="fa-regular fa-circle-check"></i>&nbsp;&nbsp;
-                                    {task.name}
-                                </button>
-                            )
-                        })}
-                        <div  className="jc-end"
-                        // style={!showAddTask ? { transform: 'translateX(+100%)' } : {}}
-                        >
-                            {showAddTask2 &&
-                            <button className=" width-100-per bg-white just-text-button round-sq-05 box-shadow b-margin">
-                        <CreateTask setShowAddTask2={setShowAddTask2} showAddTask2={showAddTask2}/>
-                        </button>
-                            }
+                    {boardView && (
+                         <div className='f width-100-per'>
+                         <div className='col width-40-per lr-margin-small'>
+                             <h2>To Do
+                                 {isMember && (
+                                  <button className='match-tasks cursor' onClick={() => (showAddTask1? setShowAddTask1(false): setShowAddTask1(true))}><i className="fa-solid fa-plus "></i></button>
+                                 )}
 
-                        </div>
-                    </div>
-                    <div className='col width-40-per lr-margin-small'>
-                        <h2>Complete
-                        {isMember && (
-                            <button className='just-text-button match-tasks' onClick={() => ( showAddTask3? setShowAddTask3(false): setShowAddTask3(true))}><i className="fa-solid fa-plus"></i></button>
-                        )}
+                                 </h2>
+                             {toDo.map(task=> {
+                                 return (
+                                     <button className="f just-text-button b-margin bg-white round-sq-05 height-task cursor pad--1" key={task.id} onClick={() => (showTaskFunc(task))}>
+                                         <i className="fa-regular fa-circle-check"></i>&nbsp;&nbsp;
+                                         {task.name}
+                                     </button>
+                                 )
+                             })}
+                             <div  className="jc-end"
+                             // style={!showAddTask ? { transform: 'translateX(+100%)' } : {}}
+                             >
+                                 {showAddTask1 &&
+                                 <button className=" width-100-per bg-white just-text-button round-sq-05 box-shadow b-margin">
+                             <CreateTask setShowAddTask1={setShowAddTask1} showAddTask1={showAddTask1}/>
+                             {/* this prop thing isnt working yet, doesnt seem to be doing anything actually */}
+                             </button>
+                                 }
 
-                            </h2>
-                        {complete.map(task=> {
-                            return (
-                                <button className="f just-text-button b-margin bg-white round-sq-05 height-task cursor pad--1" key={task.id} onClick={() =>  (showTaskFunc(task)) }>
-                                    <i className="fa-regular fa-circle-check"></i>&nbsp;&nbsp;
-                                    {task.name}
-                                </button>
-                            )
-                        })}
-                        <div  className="jc-end"
-                        // style={!showAddTask ? { transform: 'translateX(+100%)' } : {}}
-                        >
-                            {showAddTask3 &&
-                            <button className=" width-100-per bg-white just-text-button round-sq-05 box-shadow b-margin">
-                        <CreateTask setShowAddTask3={setShowAddTask3} showAddTask3={showAddTask3}/>
-                        </button>
-                            }
+                             </div>
 
-                        </div>
-
-                    </div>
-                    {showTask &&
-                     <div className='f col width-40-per bg-white box-shadow round-sq-05  ai-c'>
-                     <div  className="jc-end just-text-button b-margin bg-white round-sq-05 height-task width-90-per" style={!showTask ? { transform: 'translateX(+105%)' } : {}}>
-                         <button className=" width-100-per col just-text-button b-margin bg-white round-sq-05 height-task " >
-                         {/* onClick={() => setShowTask(false)}  add back in later*/}
-                         <ViewTask selectedTask={selectedTask}/>
-                         <EditTask selectedTask={selectedTask} showTask={showTask} setShowTask={setShowTask}/>
-                         </button>
-                     </div>
-
-                 </div>
-                    }
-
-                    </div>
+                         </div>
 
 
 
+                         <div className='col width-40-per lr-margin-small'>
+                             <h2>In Progress
+                             {isMember && (<button className='just-text-button match-tasks' onClick={() => ( showAddTask2? setShowAddTask2(false): setShowAddTask2(true))}><i className="fa-solid fa-plus"></i></button>
+                             )}
 
-                </div>
+                                 </h2>
+                             {inProg.map(task=> {
+                                 return (
+                                     <button className="f just-text-button b-margin bg-white round-sq-05 height-task cursor pad--1" key={task.id} onClick={() => (showTaskFunc(task)) }>
+                                         <i className="fa-regular fa-circle-check"></i>&nbsp;&nbsp;
+                                         {task.name}
+                                     </button>
+                                 )
+                             })}
+                             <div  className="jc-end"
+                             // style={!showAddTask ? { transform: 'translateX(+100%)' } : {}}
+                             >
+                                 {showAddTask2 &&
+                                 <button className=" width-100-per bg-white just-text-button round-sq-05 box-shadow b-margin">
+                             <CreateTask setShowAddTask2={setShowAddTask2} showAddTask2={showAddTask2}/>
+                             </button>
+                                 }
+
+                             </div>
+                         </div>
+                         <div className='col width-40-per lr-margin-small'>
+                             <h2>Complete
+                             {isMember && (
+                                 <button className='just-text-button match-tasks' onClick={() => ( showAddTask3? setShowAddTask3(false): setShowAddTask3(true))}><i className="fa-solid fa-plus"></i></button>
+                             )}
+
+                                 </h2>
+                             {complete.map(task=> {
+                                 return (
+                                     <button className="f just-text-button b-margin bg-white round-sq-05 height-task cursor pad--1" key={task.id} onClick={() =>  (showTaskFunc(task)) }>
+                                         <i className="fa-regular fa-circle-check"></i>&nbsp;&nbsp;
+                                         {task.name}
+                                     </button>
+                                 )
+                             })}
+                             <div  className="jc-end"
+                             // style={!showAddTask ? { transform: 'translateX(+100%)' } : {}}
+                             >
+                                 {showAddTask3 &&
+                                 <button className=" width-100-per bg-white just-text-button round-sq-05 box-shadow b-margin">
+                             <CreateTask setShowAddTask3={setShowAddTask3} showAddTask3={showAddTask3}/>
+                             </button>
+                                 }
+
+                             </div>
+
+                         </div>
+                         {showTask &&
+                          <div className='f col width-40-per bg-white box-shadow round-sq-05  ai-c'>
+                          <div  className="jc-end just-text-button b-margin bg-white round-sq-05 height-task width-90-per" style={!showTask ? { transform: 'translateX(+105%)' } : {}}>
+                              <button className=" width-100-per col just-text-button b-margin bg-white round-sq-05 height-task " >
+                              {/* onClick={() => setShowTask(false)}  add back in later*/}
+                              <ViewTask selectedTask={selectedTask}/>
+                              <EditTask selectedTask={selectedTask} showTask={showTask} setShowTask={setShowTask}/>
+                              </button>
+                          </div>
+
+                      </div>
+                         }
+
+                         </div>
 
 
-                {/* <div className='border-orange col'>
-                    TASKS:
-                    {oneProject.tasks.map(task => {
-                    return (
-                        <div key={task.id} className='border-blue'>
-                        <div>{task.id}. {task.name} Stage: {task.stageId}</div>
-                        <br/>
-                    </div>
-                        )})}
-                </div> */}
+                    )}
+                       </div>
+
 
 
         </div>
